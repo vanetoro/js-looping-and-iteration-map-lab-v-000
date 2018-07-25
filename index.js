4a5b9c1b5de222f8feb258e82ce11e4e529bb692
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 19:17:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,11 @@
 function lowerCaseDrivers(arr){
     return arr.map(function(name) {return name.toLowerCase() })
 }
+
+
+function nameToAttributes(arr){
+  return arr.map(function(name){
+        return {firstName: name.split(' ')[0],
+                lastName: name.split(' ')[1]}
+  })
+}
