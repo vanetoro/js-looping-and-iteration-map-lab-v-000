e26f50066bf7d896de9e289b7d7e18e93992f50a
Vanessa Toro <vanetoro84@gmail.com>
2018-07-25 19:00:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file.
 function lowerCaseDrivers(arr){
-    return arr.map(function(name) {name.toLowerCase()})
+    return arr.map(function(name) {return name.toLowerCase() })
 }
